8ff27f0055f91c5fe5c049b1528f35c26a46f78b
SanjaySaatyaki <sanjayabs97@gmail.com>
2025-12-20 20:09:29 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # learn_ray
+
+run in docker
+
+- docker pull rayproject/ray:latest-cpu
+- docker run -it --shm-size=2g -p 8265:8265 -p 8000:8000 rayproject/ray:latest-cpu bash
---
+++ job_submission.py
@@ -0,0 +1,13 @@
+from ray.job_submission import JobSubmissionClient
+
+# If using a remote cluster, replace 127.0.0.1 with the head node's IP address or set up port forwarding.
+client = JobSubmissionClient("http://127.0.0.1:8265")
+job_id = client.submit_job(
+    # Entrypoint shell command to execute
+    entrypoint="python bert_training.py",
+    # Path to the local directory that contains the script.py file
+    runtime_env={"working_dir": "./",
+                 "pip":["transformers","ray","evaluate"]},
+
+)
+print(job_id)
